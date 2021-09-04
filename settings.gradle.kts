3b3c10d469a7c479f3f4550ad3097d6b1d6b65fc
Chris Jaehnen <cjaehnen@me.com>
2021-09-04 16:39:52 -0400

    Make project sync changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("de.fayard.refreshVersions") version "0.10.1"
-////                            # available:"0.11.0"
-////                            # available:"0.20.0"
+    id("de.fayard.refreshVersions") version "0.20.0"
 }
 
 rootProject.name = "settings-gradle-plugin"
