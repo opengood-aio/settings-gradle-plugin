6239239cff2de4dce6ec35018783a1d00f384b27
Chris Jaehnen <cjaehnen@me.com>
2023-06-13 20:49:05 -0400

    Update build Gradle for Gradle Release plugin 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-import net.researchgate.release.GitAdapter.GitConfig
-import net.researchgate.release.ReleaseExtension
+
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
 import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
@@ -192,9 +191,6 @@ with(tasks) {
         dependsOn("publishPlugins")
     }
 }
-
-fun ReleaseExtension.git(config: GitConfig.() -> Unit) =
-    (getProperty("git") as GitConfig).config()
 
 release {
     preTagCommitMessage.set("[Gradle Release] - pre tag commit: ")
